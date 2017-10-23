9ccd129e7b842b3a8e102ab91f8d31d90b17b8e0
cutrain <454615501@qq.com>
2017-10-23 21:37:42 +0800

    fix: change func "doo" using "eval"

--- config.sh
+++ config.sh
@@ -7,27 +7,27 @@ ct() {
 ef="ct 32 Succeed"
 er="ct 31 Failed"
 doo() {
-	ct 33 "running $*"
-	$* && $ef || $er
+	ct 33 "running '$*'"
+	eval "$*" && $ef || $er
 }
 
 sudo ct 32 "start"
 ct 36 "Copy config files"
-doo cp .tmux.conf ~
-doo cp .vimrc ~
+doo "cp .tmux.conf ~"
+doo "cp .vimrc ~"
 ct 36 "Install workenv tools"
-doo sudo apt-get install -y vim git tmux zsh curl build-essential cmake python-dev python3-dev python-pip python3-pip
+doo "sudo apt-get install -y vim git tmux zsh curl build-essential cmake python-dev python3-dev python-pip python3-pip"
 ct 36 "Downloading zsh config"
-doo sudo sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
-doo cp .zshrc ~/.zshrc
+doo 'sudo sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"'
+doo 'cp .zshrc ~/.zshrc'
 ct 36 "Config vim plugins"
-doo git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
-doo vim -c BundleInstall -c qa ~/.vimrc
+doo 'git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim'
+doo 'vim -c BundleInstall -c qa ~/.vimrc'
 
 # The following is installing the plugin YouCompleteMe, it will spend a lot of time, if you don't have enough time, delete it in .vimrc
 ct 33 "It's going to config YouCompleteMe, it will spend lots of time, you can stop it using Ctrl+C"
 ct 36 "Configing YouCompleteMe..."
-doo cp .ycm_extra_conf.py ~/.vim/bundle/YouCompleteMe
-doo cd ~/.vim/bundle/YouCompleteMe/
-doo ./install.py --clang-completer
-doo pip install jedi
+doo 'cp .ycm_extra_conf.py ~/.vim/bundle/YouCompleteMe'
+doo 'cd ~/.vim/bundle/YouCompleteMe/'
+doo './install.py --clang-completer'
+doo 'pip install jedi'
