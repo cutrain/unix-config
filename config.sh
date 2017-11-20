0c9493bb807d2dd4e2dd6acbbea7217c108609d3
cutrain <454615501@qq.com>
2017-11-20 21:48:59 +0800

    add npm & node install

--- config.sh
+++ config.sh
@@ -15,17 +15,27 @@ cmt() {
 }
 
 sudo ct 32 "start"
+
 cmt "Copy config files"
 doo "cp .tmux.conf ~"
 doo "cp .vimrc ~"
+
 cmt "Install workenv tools"
 doo "sudo apt-get install -y vim git tmux zsh curl build-essential cmake python-dev python3-dev python-pip python3-pip"
+
 cmt "Downloading zsh config"
 doo 'sudo sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"'
 doo 'cp .zshrc ~/.zshrc'
+
 cmt "Config vim plugins"
 doo 'git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim'
 doo 'vim -c BundleInstall -c qa ~/.vimrc'
+
+cmt "config node denpency"
+doo "wget -qO- https://raw.githubusercontent.com/creationix/nvm/v0.33.6/install.sh | bash"
+doo "nvm install 0.10"
+doo "npm install ffi"
+doo "npm install ref"
 
 # The following is installing the plugin YouCompleteMe, it will spend a lot of time, if you don't have enough time, delete it in .vimrc
 ct 33 "It's going to config YouCompleteMe, it will spend lots of time, you can stop it using Ctrl+C"
