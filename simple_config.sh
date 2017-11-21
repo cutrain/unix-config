f5028eb15d67641d85d0168faff441bc86ca2863
cutrain <454615501@qq.com>
2017-11-21 05:52:47 +0000

    fix: change the order

--- simple_config.sh
+++ simple_config.sh
@@ -23,11 +23,11 @@ doo "cp .vimrc ~"
 cmt "Install workenv tools"
 doo "sudo apt-get install -y vim git tmux zsh curl python-dev python-pip"
 
-cmt "Downloading zsh config"
-doo 'sudo sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"'
-doo 'cp .zshrc ~/.zshrc'
-
 cmt "Config vim plugins"
 doo 'git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim'
 doo 'vim -c BundleInstall -c qa ~/.vimrc'
+
+cmt "Downloading zsh config"
+doo 'sudo sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"'
+doo 'cp .zshrc ~/.zshrc'
 
