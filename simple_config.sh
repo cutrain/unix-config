ecc976407e9f570697ed92e0a5cd8125ccafb300
cutrain <454615501@qq.com>
2018-05-07 19:33:46 +0800

    feat: just for ZTJ

--- simple_config.sh
+++ simple_config.sh
@@ -21,7 +21,10 @@ doo "cp .tmux.conf ~"
 doo "cp .vimrc_simple ~/.vimrc"
 
 cmt "Install workenv tools"
-doo "sudo apt-get install -y vim git tmux zsh curl python-dev python-pip"
+doo "sudo apt-get install -y vim git tmux zsh curl build-essential cmake python-dev python3-dev python-pip python3-pip"
+doo "cp -r .pip ~"
+doo "git config --global core.editor vim"
+doo "git config --global push.default simple"
 
 cmt "Config vim plugins"
 doo 'git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim'
@@ -30,4 +33,11 @@ doo 'vim -c BundleInstall -c qa ~/.vimrc'
 cmt "Downloading zsh config"
 doo 'sudo sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"'
 doo 'cp .zshrc ~/.zshrc'
+doo 'cp .zprofile ~/.zprofile'
 
+ct 33 "It's going to config YouCompleteMe, it will spend lots of time, you can stop it using Ctrl+C"
+cmt "Configing YouCompleteMe..."
+doo 'cp .ycm_extra_conf.py ~/.vim/bundle/YouCompleteMe/third_party/ycmd/cpp/ycm/.ycm_extra_conf.py'
+doo 'sudo apt-get install -y g++-5'
+doo 'cd ~/.vim/bundle/YouCompleteMe/'
+doo './install.py --clang-completer'
