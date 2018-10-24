18a2996a11d12e881f14a5bc230dd9988f17c2c0
cutrain <454615501@qq.com>
2018-10-24 09:35:36 +0800

    feat: add ssh-server

--- basic.sh
+++ basic.sh
@@ -12,4 +12,4 @@ cmt() {
 	ct 36 $*
 }
 cmt 'Install basic tools'
-doo 'sudo apt-get install -y vim git tmux zsh curl build-essential cmake python-dev python3-dev python-pip python3-pip autojump g++-7'
+doo 'sudo apt-get install -y vim git tmux zsh curl build-essential cmake python-dev python3-dev python-pip python3-pip autojump g++-7 openssh-server'
