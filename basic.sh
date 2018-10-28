db03af1812efc8d71821ba898e01bf4e9b0b7251
cutrain <454615501@qq.com>
2018-10-28 13:48:52 +0800

    feat: delete g++ preinstall

--- basic.sh
+++ basic.sh
@@ -12,4 +12,5 @@ cmt() {
 	ct 36 $*
 }
 cmt 'Install basic tools'
-doo 'sudo apt-get install -y vim git tmux zsh curl build-essential cmake python-dev python3-dev python-pip python3-pip autojump g++-7 openssh-server'
+doo 'sudo apt-get update'
+doo 'sudo apt-get install -y vim git tmux zsh curl build-essential cmake python-dev python3-dev python-pip python3-pip autojump openssh-server'
