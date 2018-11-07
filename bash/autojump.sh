ae3024b8c2ad280abc534f0a6073849dd3a753fd
cutrain <454615501@qq.com>
2018-11-08 03:13:54 +0800

    bug: fix autojump

--- bash/autojump.sh
+++ bash/autojump.sh
@@ -1,7 +1,22 @@
 #!/bin/bash
-git clone git://github.com/joelthelion/autojump.git
-cd autojump
-./install.sh
-echo '[[ -s $HOME/.autojump/etc/profile.d/autojump.sh ]] && source $HOME/.autojump/etc/profile.d/autojump.sh' >> ~/.zshrc
-echo 'autoload -U compinit && compinit -u' >> ~/.zshrc
+ct() {
+	echo -ne "\033[$1;1m"
+	echo -n $2
+	echo -e "\033[0m"
+}
+ef="ct 32 Succeed"
+er="ct 31 Failed"
+doo() {
+	ct 33 "running '$*'"
+	eval "$*" && $ef || $er
+}
+cmt() {
+	ct 36 $*
+}
 
+doo 'git clone git://github.com/joelthelion/autojump.git'
+doo 'cd autojump'
+doo './install.py'
+doo 'echo '"'"'[[ -s $HOME/.autojump/etc/profile.d/autojump.sh ]] && source $HOME/.autojump/etc/profile.d/autojump.sh'"'"' >> ~/.zshrc'
+doo 'echo '"'"'autoload -U compinit && compinit -u'"'"' >> ~/.zshrc'
+doo 'source ~/.zshrc'
