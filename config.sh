fd3beed7b25ce6769f47bc6a031973607731a874
cutrain <duanyuge@qq.com>
2018-12-18 09:07:09 +0000

    feat: ignore installed oh-my-zsh & Vundle

--- config.sh
+++ config.sh
@@ -10,14 +10,23 @@ cmt 'config git'
 doo 'cp ./config/git/.gitconfig ~'
 doo 'cp ./config/git/.gitmessage ~'
 
-cmt 'Downloading zsh config'
-doo 'sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)" <<< exit'
+cmt 'Installing zsh'
+if [ -z '~/.oh-my-zsh' ] ;then
+	cmt 'Downloading oh-my-zsh config'
+	doo 'sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)" <<< exit'
+else
+	cmt 'NOTE: You have already installed oh-my-zsh, if you want reinstall it, remove "~/.oh-my-zsh"'
+fi
 
-cmt 'setting zsh'
+
+cmt 'Setting up zsh'
 doo 'cp ./config/zsh/.zshrc ~/.zshrc'
 doo 'cp ./config/zsh/.zprofile ~/.zprofile'
 
 cmt 'Config vim plugins'
-doo 'git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim'
+if [ -z '~/.vim/bundle/Vundle.vim' ] ;then
+	cmt 'Cloning Vundle'
+	doo 'git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim'
+fi
 doo 'vim -c BundleInstall -c qa ~/.vimrc'
 
