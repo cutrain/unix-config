2cc8146917b474ba0164446a82bbf7ed3cd68e63
cutrain <duanyuge@qq.com>
2018-12-26 15:38:41 +0800

    fix: fix ycm install script

--- extra.sh
+++ extra.sh
@@ -2,12 +2,13 @@
 source ./tools/common.sh
 
 cmt 'Config vim plugin : YouCompleteMe'
-doo 'vim -c %s/vundle#begin()/vundle#begin()\rBundle '"'"'Valloric\/YouCompleteMe'"'"' -c wq ~/.vimrc'
+doo 'vim -c '"'"'%s/vundle#begin()/vundle#begin()\rPlugin\ '"'"'"'"'"'"'"'"'Valloric\/YouCompleteMe\'"'"'"'"'"'" -c wq ~/.vimrc'
 doo 'vim -c BundleInstall -c qa'
 doo 'cp ./config/vim/.ycm_extra_conf.py ~/.vim/bundle/YouCompleteMe/.ycm_extra_conf.py'
 doo 'cd ~/.vim/bundle/YouCompleteMe/'
 doo 'python3 install.py --clang-completer'
 doo 'pip3 install jedi'
+doo 'cd -'
 
 
 cmt 'Config shadowsocks client'
