dde22e873c5e8b6acef596ce5934559d54c8c049
cutrain <duanyuge@qq.com>
2018-12-26 15:47:57 +0800

    feat: fudan internet script

--- tools/fd-net-auth.sh
+++ tools/fd-net-auth.sh
@@ -2,7 +2,7 @@
 
 URL="https://10.108.255.249/include/auth_action.php"
 username=18210240005
-password=239358
+read -s -p "Enter password:" password
 ip=10.141.2.231
 
 curl $URL --insecure --data "action=login&username=$username&password=$password&ac_id=1&user_ip=$ip&nas_ip=&user_mac=&save_me=1&ajax=1"
