3a5d45af71ed5358b76447248655ca77db6c40e3
cutrain <duanyuge@qq.com>
2019-09-23 21:02:15 +0800

    Feature: delete GoQuiet(shadowsocks plugin)
    Use DDNS instead

--- bash/proxy_client.sh
+++ bash/proxy_client.sh
@@ -12,8 +12,3 @@ doo 'pip install shadowsocks'
 doo "(echo $PATH | grep "$HOME/.local/bin") || (mkdir -p $INSTALL_DIR && (echo 'export PATH=$HOME/.local/bin:$PATH' >> $BASH_FILE))"
 doo "cp ../config/proxy/ss_client $INSTALL_DIR"
 doo "cp ../config/proxy/.shadowsocks_client.json $HOME"
-doo "cp ../config/proxy/.gq_client.json $HOME"
-
-cmt 'If you can`t use gq-client, please download from "https://github.com/cbeuw/GoQuiet/releases" for proper version, watching architecture by command "uname -m"'
-doo 'wget -c https://github.com/cbeuw/GoQuiet/releases/download/v1.2.2/gq-client-linux-386-1.2.2 -O gq-client'
-doo "mv gq-client $INSTALL_DIR"
--- bash/proxy_server.sh
+++ bash/proxy_server.sh
@@ -12,8 +12,3 @@ doo 'pip install shadowsocks'
 doo "(echo $PATH | grep "$HOME/.local/bin") || (mkdir -p $INSTALL_DIR && (echo 'export PATH=$HOME/.local/bin:$PATH' >> $BASH_FILE))"
 doo "cp ../config/proxy/ss_server $INSTALL_DIR"
 doo "cp ../config/proxy/.shadowsocks_server.json $HOME"
-doo "cp ../config/proxy/.gq_server.json $HOME"
-
-cmt 'If you can`t use gq-server, please download from "https://github.com/cbeuw/GoQuiet/releases" for proper version, watching architecture by command "uname -m"'
-doo 'wget -c https://github.com/cbeuw/GoQuiet/releases/download/v1.2.2/gq-server-linux-386-1.2.2 -O gq-server'
-doo "mv gq-server $INSTALL_DIR"
